4bebc79047e3a3d5ae8c1d85a7308aec665d77d4
Elton <seuemail@gmail.com>
2026-04-12 22:56:53 -0300

    corrigindo rota inicial pwa

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def index():
         return render_template("login.html")
     return render_template("index.html")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
